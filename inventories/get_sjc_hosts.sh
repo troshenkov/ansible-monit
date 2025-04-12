8c42b2a1fd4a5d88af12a7f1a1829f0ccffbb446
Dmitry Troshenkov <troshenkov.d@gmail.com>
2025-04-12 03:44:01 -0700

    Refactor get_sjc_hosts.sh: improve modularity, add detailed documentation, enhance readability, and implement better error handling.

--- inventories/get_sjc_hosts.sh
+++ inventories/get_sjc_hosts.sh
@@ -1,54 +1,95 @@
 #!/bin/bash
-#
-# Dmitry Troshenkov (troshenkov.d@gmail.com)
 
+# ------------------------------------------------------------------------------
+# Script Name: get_sjc_hosts.sh
+# Author: Dmitry Troshenkov (troshenkov.d@gmail.com)
+# Description:
+#   This script retrieves host information from the LSF farm and updates an 
+#   inventory file. Hosts are categorized into predefined groups based on their 
+#   naming conventions, and their statuses are updated accordingly.
+#
+# Usage:
+#   ./get_sjc_hosts.sh
+#
+# Configuration:
+#   - INVENTORY: Path to the inventory file to be updated.
+#
+# Dependencies:
+#   - bhosts: Command-line tool to fetch host information from the LSF farm.
+#   - sed, awk, date: Standard Linux utilities used for text processing.
+#
+# Notes:
+#   - Ensure the script has the necessary permissions to modify the inventory file.
+#   - The script assumes the LSF farm is accessible and the `bhosts` command is 
+#     available in the system's PATH.
+#
+# Exit Codes:
+#   0 - Success
+#   1 - General error
+#   2 - No data loaded from the LSF farm
+# ------------------------------------------------------------------------------
+
+# Configuration
 INVENTORY='hosts'
 
-# Add the hosts and a host status: ok, closed, unavail
-declare -a LSF_FARM=($(/auto/edatools/bin/bhosts --farm sjc-hw|grep -v HOST \
-                                   | awk '{ print $1 "&host_status=" $2 }'))
-
-if [ ${#LSF_FARM[@]} -gt 0 ]; then
-    # Clean an inventory file
+# Function to clean the inventory file
+clean_inventory() {
+    # Remove existing host entries
     sed -i -r "/^[a-z]{3}-[a-z]{2,3}-[0-9]{3,4}/d" "$INVENTORY"
     sed -i -r "/^[a-z]{3}-[a-z]{2}-master[0-9]/d" "$INVENTORY"
-    # Make a new date yy/mm/dd hh:mm:ss AM/PM ZZZ
-    _DATA='[0-9]{2}/[0-9]{2}/[0-9]{2}?.[0-9]{2}:[0-9]{2}:[0-9]{2}?.[A-Z]{2}?.[A-Z]{3}'
-    sed -i -r "s|"$_DATA"|$(date +"%y\/%m\/%d\&%I:%M:%S\&%p\&%Z")|g" "$INVENTORY"
-  else
-      echo "No data has been loaded."; exit 2
-fi
 
-# Add the new hosts in to inventory file
-for ((n=0; n < ${#LSF_FARM[*]}; n++)) ; do
-# [sjc-hw-masters]
-    if   [[ ${LSF_FARM[$n]} =~ ^sjc-hw-master[0-9]{1} ]]; then
-        sed -i -e "/\[sjc\-hw\-masters\]/a"$'\\\n'"${LSF_FARM[$n]}"$'\n' "$INVENTORY"
-# [dsw-lnx]
-    elif [[ ${LSF_FARM[$n]} =~ ^dsw-lnx-[0-9]{3,4} ]]; then
-        sed -i -e "/\[dsw\-lnx\]/a"$'\\\n'"${LSF_FARM[$n]}"$'\n' "$INVENTORY"
-# [eag-lnx]
-    elif [[ ${LSF_FARM[$n]} =~ ^eag-lnx-[0-9]{3,4} ]]; then
-        sed -i -e "/\[eag\-lnx\]/a"$'\\\n'"${LSF_FARM[$n]}"$'\n' "$INVENTORY"
-# [rtg-lnx]
-    elif [[ ${LSF_FARM[$n]} =~ ^rtg-lnx-[0-9]{3,4} ]]; then
-        sed -i -e "/\[rtg\-lnx\]/a"$'\\\n'"${LSF_FARM[$n]}"$'\n' "$INVENTORY"
-# [rtp-hw]
-    elif [[ ${LSF_FARM[$n]} =~ ^rtp-hw-[0-9]{3,4} ]]; then
-        sed -i -e "/\[rtp\-hw\]/a"$'\\\n'"${LSF_FARM[$n]}"$'\n' "$INVENTORY"
-# [rtp-lnx]
-    elif [[ ${LSF_FARM[$n]} =~ ^rtp-lnx-[0-9]{3,4} ]]; then
-        sed -i -e "/\[rtp\-lnx\]/a"$'\\\n'"${LSF_FARM[$n]}"$'\n' "$INVENTORY"
-# [sjc-hw]
-    elif [[ ${LSF_FARM[$n]} =~ ^sjc-hw-[0-9]{3,4} ]]; then
-        sed -i -e "/\[sjc\-hw\]/a"$'\\\n'"${LSF_FARM[$n]}"$'\n' "$INVENTORY"
-    else
-        echo "No matching for the hosts group found: " ${LSF_FARM[$n]}
+    # Update the date in the inventory file
+    local date_pattern='[0-9]{2}/[0-9]{2}/[0-9]{2}?.[0-9]{2}:[0-9]{2}:[0-9]{2}?.[A-Z]{2}?.[A-Z]{3}'
+    sed -i -r "s|$date_pattern|$(date +"%y/%m/%d&%I:%M:%S&%p&%Z")|g" "$INVENTORY"
+}
+
+# Function to add a host to the appropriate group
+add_host_to_group() {
+    local host="$1"
+    local group="$2"
+    sed -i -e "/\[$group\]/a"$'\\\n'"$host"$'\n' "$INVENTORY"
+}
+
+# Main script execution
+main() {
+    # Fetch hosts and their statuses from the LSF farm
+    declare -a LSF_FARM=($(/auto/edatools/bin/bhosts --farm sjc-hw | grep -v HOST | awk '{ print $1 "&host_status=" $2 }'))
+
+    # Exit if no data is loaded
+    if [ ${#LSF_FARM[@]} -eq 0 ]; then
+        echo "No data has been loaded."
+        exit 2
     fi
-done
 
-# Remove the separator '&'
-sed -i "s/\&/ /g" "$INVENTORY"
+    # Clean the inventory file
+    clean_inventory
+
+    # Add hosts to their respective groups
+    for host_entry in "${LSF_FARM[@]}"; do
+        if [[ $host_entry =~ ^sjc-hw-master[0-9]{1} ]]; then
+            add_host_to_group "$host_entry" "sjc-hw-masters"
+        elif [[ $host_entry =~ ^dsw-lnx-[0-9]{3,4} ]]; then
+            add_host_to_group "$host_entry" "dsw-lnx"
+        elif [[ $host_entry =~ ^eag-lnx-[0-9]{3,4} ]]; then
+            add_host_to_group "$host_entry" "eag-lnx"
+        elif [[ $host_entry =~ ^rtg-lnx-[0-9]{3,4} ]]; then
+            add_host_to_group "$host_entry" "rtg-lnx"
+        elif [[ $host_entry =~ ^rtp-hw-[0-9]{3,4} ]]; then
+            add_host_to_group "$host_entry" "rtp-hw"
+        elif [[ $host_entry =~ ^rtp-lnx-[0-9]{3,4} ]]; then
+            add_host_to_group "$host_entry" "rtp-lnx"
+        elif [[ $host_entry =~ ^sjc-hw-[0-9]{3,4} ]]; then
+            add_host_to_group "$host_entry" "sjc-hw"
+        else
+            echo "No matching group found for host: $host_entry"
+        fi
+    done
+
+    # Remove the separator '&' from the inventory file
+    sed -i "s/\&/ /g" "$INVENTORY"
+}
+
+# Execute the main function
+main
 
 exit 0
-
